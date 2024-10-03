2ae782cd3b27a560e18527eb3a25e768058669b8
Matt Bannister <matt.bann@live.co.uk>
2024-10-03 21:16:23 +0100

    Minor changes

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,7 +11,7 @@ std::unique_ptr<NetworkHandler> p_networkHandler;
 
 std::atomic<bool> returnThreads{false};
 
-const std::string hostname = "192.168.1.114";
+const std::string hostname = "31.125.190.62";
 const std::string port = "27011";
 
 // UpdateRate
